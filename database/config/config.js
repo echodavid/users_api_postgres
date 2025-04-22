d967f8679e453aadb4e73a0592b74fc3afe045af
echodavid <despinosa2550@gmail.com>
2025-04-22 04:25:54 -0600

    passing ssl

--- database/config/config.js
+++ database/config/config.js
@@ -5,15 +5,33 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     url: process.env.DATABASE_URL || 'postgres://root:password@127.0.0.1:5432/database_development',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Cambiar a true si tienes un certificado válido
+      },
+    },
   },
   test: {
     use_env_variable: 'TEST_DATABASE_URL',
     url: process.env.TEST_DATABASE_URL || 'postgres://root:password@127.0.0.1:5432/database_test',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     use_env_variable: 'PROD_DATABASE_URL',
     url: process.env.PROD_DATABASE_URL || 'postgres://root:password@127.0.0.1:5432/database_production',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
