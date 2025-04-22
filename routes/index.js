7dac1e84b3e9db1cf372a04ccf8484cf02062d4c
echodavid <despinosa2550@gmail.com>
2025-04-22 05:05:00 -0600

    changin route order

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome to the API!'));
 
-router.post('/users', controllers.createUser);
-router.get('/users', controllers.getAllUsers);
 router.get('/users/:id', controllers.getUserById);
 router.put('/users/:id', controllers.updateUser);
+router.post('/users', controllers.createUser);
+router.get('/users', controllers.getAllUsers);
 router.delete('/users/:id', controllers.deleteUser);
 
 
